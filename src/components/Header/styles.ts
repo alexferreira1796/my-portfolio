79d418092774fbefeface267a66de5492972d790
Alex Ferreira <alex.ferreira1796@gmail.com>
2021-10-18 23:18:07 -0300

    ajuste no responsivo menu

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,10 +1,14 @@
 import styled from 'styled-components';
 
 export const Header = styled.header`
-    width: 100%;
-    height: auto;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-top: 10px;
-`
+  width: 100%;
+  height: auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 10px;
+
+  @media screen and (max-width: 800px) {
+    align-items: initial;
+  }
+`;
